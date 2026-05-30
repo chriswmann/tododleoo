cd4f6e1ef3bed90d690dc25b68091c4964c55e24
Chris <8133496+chriswmann@users.noreply.github.com>
2026-05-30 16:24:45 +0100

    Wire persistence into Main

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,13 @@
 module Main (main) where
 
+import Control.Monad (when)
+import Data.Char (toLower)
 import qualified Data.Text as T
 import Data.Time (getCurrentTime)
 import Domain (Store, completeTodo, createTodo, deleteTodo, emptyStore, getTodo, insertTodo, parseTodoTitle)
+import Persistence (defaultStorePath, loadStore, saveStore)
 import System.Directory (doesFileExist)
+import System.Exit (exitFailure, exitSuccess)
 import Text.Read (readMaybe)
 
 data StoreCommand
@@ -66,19 +70,54 @@ execute command store = case command of
 printMenu :: IO ()
 printMenu = putStrLn "Commands: add, done, remove, view, list, help, quit"
 
-loop :: Store -> IO ()
-loop store = do
+loop :: FilePath -> Store -> IO ()
+loop path store = do
   printMenu
   input <- getLine
   case parseCommand input of
-    Nothing -> do putStrLn "Unknown command"; loop store
-    Just (Meta Help) -> do printMenu; loop store
+    Nothing -> do putStrLn "Unknown command"; loop path store
+    Just (Meta Help) -> do printMenu; loop path store
     Just (Meta Quit) -> putStrLn "Bye!"
     Just (Store command) -> do
       updatedStore <- execute command store
-      loop updatedStore
+      when (updatedStore /= store) (saveStore path updatedStore)
+      loop path updatedStore
+
+data YesOrNo = Yes | No
+  deriving (Show, Eq)
+
+parseYesOrNo :: String -> Maybe YesOrNo
+parseYesOrNo input = case map toLower input of
+  "y" -> Just Yes
+  "n" -> Just No
+  _ -> Nothing
+
+confirmNewPath :: FilePath -> IO Bool
+confirmNewPath fp = do
+  putStrLn $ "The given store path, " ++ fp ++ ", does not exist. Do you want to create it? (y/n): "
+  input <- getLine
+  case parseYesOrNo input of
+    Just Yes -> pure True
+    Just No -> pure False
+    Nothing -> confirmNewPath fp
+
+loadExisting :: FilePath -> IO Store
+loadExisting fp = do
+  result <- loadStore fp
+  case result of
+    Left err -> do
+      putStrLn ("Could not load store at: " ++ fp ++ ": " ++ show err)
+      exitFailure
+    Right store -> pure store
 
 main :: IO ()
 main = do
-  let store = emptyStore
-  loop store
+  path <- defaultStorePath
+  exists <- doesFileExist path
+  store <-
+    if exists
+      then loadExisting path
+      else do
+        create <- confirmNewPath path
+        if create then pure emptyStore else exitSuccess
+  loop path store
