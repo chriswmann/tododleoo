1ff50784f8af1f140481ad030a820dc1420cb6c4
Chris <8133496+chriswmann@users.noreply.github.com>
2026-05-30 13:54:18 +0100

    Add Persistence.hs to handle saving and loading the store

---
+++ src/Persistence.hs
@@ -0,0 +1,30 @@
+module Persistence where
+
+import Data.Aeson (eitherDecode, eitherDecodeFileStrict, eitherDecodeFileStrict', encode)
+import Data.Aeson.Encode.Pretty (encodePretty)
+import qualified Data.ByteString.Lazy as BL
+import Domain (Store)
+import System.Directory (XdgDirectory (XdgData), createDirectoryIfMissing, getXdgDirectory, renameFile)
+import System.FilePath (takeDirectory, (</>))
+
+data LoadError = FileCorruptedError String
+  deriving (Show, Eq)
+
+defaultStorePath :: IO FilePath
+defaultStorePath = do
+  dir <- getXdgDirectory XdgData "todoleoo"
+  pure (dir </> "todoleoos.json")
+
+loadStore :: FilePath -> IO (Either LoadError Store)
+loadStore fp = do
+  result <- eitherDecodeFileStrict' fp
+  case result of
+    Left err -> pure (Left (FileCorruptedError err))
+    Right store -> pure (Right store)
+
+saveStore :: FilePath -> Store -> IO ()
+saveStore fp s = do
+  createDirectoryIfMissing True (takeDirectory fp)
+  let tmp = fp ++ ".tmp"
+  BL.writeFile tmp (encodePretty s)
+  renameFile tmp fp
